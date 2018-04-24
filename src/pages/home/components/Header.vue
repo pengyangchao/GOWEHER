4dd36a568b6f00a7a6c501708904156ba73bb31c
pengyangchao <pengyangchao@jiuchunjiaoyu.com>
2018-04-24 17:32:56 +0800

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+<div class='header'>
+  <div class='header-left'>
+    <span class="iconfont">&#xe624;</span>
+  </div>
+  <div class='header-input'>
+    <span class="iconfont">&#xe632;</span>
+  </div>
+  <div class='header-right'>城市
+    <span class="iconfont">&#xe64a;</span>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~styles/varibles.styl';
+  .header
+    display flex
+    flex-directio row
+    line-height .86rem
+    background-color $bgColor
+    color #fff
+  .header-left
+    width .64rem
+    text-align center
+  .header-input
+    flex 1
+    background-color #fff
+    margin-top .12rem
+    margin-left .2rem
+    height .64rem
+  .header-right
+    width 1.24rem
+    text-align center
+</style>
